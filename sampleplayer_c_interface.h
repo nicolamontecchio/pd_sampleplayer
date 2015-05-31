dddf7e294ed0897ba6cad5f59e4caa632f0272ce
Nicola Montecchio <nicola.montecchio@gmail.com>
2015-05-31 11:24:52 -0400

    tabs

--- sampleplayer_c_interface.h
+++ sampleplayer_c_interface.h
@@ -1,19 +1,17 @@
 #ifdef __cplusplus
-extern "C" 
+extern "C"
 {
-#endif 
+#endif
 
-
-	void* new_sampleplayer_obj();
-	void free_sampleplayer_obj(void* o);
-	int sampleplayer_initialize(void* o); // for now just return the number of non-loaded samples
-	void sampleplayer_set_sample(void* o, int pitch, char* file_path, int release);
-	void sampleplayer_voice_on(void* o, int voice, int sample_id, float intensity);
-	void sampleplayer_voice_off(void *o, int voice);
-	void sampleplayer_tick(void *o, float** out, int out_channels, int nsamples);
-	int sampleplayer_memoryusage(void *o);
+  void* new_sampleplayer_obj();
+  void free_sampleplayer_obj(void* o);
+  int sampleplayer_initialize(void* o); // for now just return the number of non-loaded samples
+  void sampleplayer_set_sample(void* o, int pitch, char* file_path, int release);
+  void sampleplayer_voice_on(void* o, int voice, int sample_id, float intensity);
+  void sampleplayer_voice_off(void *o, int voice);
+  void sampleplayer_tick(void *o, float** out, int out_channels, int nsamples);
+  int sampleplayer_memoryusage(void *o);
 
 #ifdef __cplusplus
 }
 #endif
-
