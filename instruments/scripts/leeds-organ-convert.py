362ec1a7416a4fcc5c370c419de9645b3302ed7f
nicola montecchio <nicola.montecchio@gmail.com>
2015-12-08 23:40:31 -0500

    parse only first set of samples

--- instruments/scripts/leeds-organ-convert.py
+++ instruments/scripts/leeds-organ-convert.py
@@ -5,19 +5,23 @@ import re
 import os
 
 # http://store.samplephonics.com/products/the-leeds-town-hall-organ
+# N.B. this just parses the first set of samples for each preset, discards the second
 
 
 def parse_preset(fstream_in):
     # line stream to tuples (pitch, filename, loop_start, loop_end)
     def clamp(p):
-        return str(max(12, min(96, int(p))))
+        return max(12, min(96, int(p)))
+    last_pitch = -1
     for line in fstream_in:
         if line.startswith('<region>'):
             pitch      = clamp(re.search('lokey=(\d+) ', line).group(1))
             filename   = re.search('sample=([^ ]+) ', line).group(1)
             loop_start = re.search('loopstart=(\d+) ', line).group(1)
             loop_end   = re.search('loopend=(\d+) ', line).group(1)
-            yield (pitch, filename, loop_start, loop_end)
+            if pitch > last_pitch:
+                yield (str(pitch), filename, loop_start, loop_end)
+                last_pitch = pitch
 
 
 if __name__ == '__main__':
