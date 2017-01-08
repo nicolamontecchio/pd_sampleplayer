ca7f1c8e47c4608b72ce915c88a5743b5ebb4203
nicola montecchio <nicola.montecchio@gmail.com>
2017-01-08 13:16:14 +0100

    loops are 1s long

--- instruments/scripts/burea_funeral_chapel_convert.py
+++ instruments/scripts/burea_funeral_chapel_convert.py
@@ -15,10 +15,13 @@ if __name__ == '__main__':
 
     _, dir_in = sys.argv
     dir_out = 'instruments/burea'
+    os.mkdir(dir_out)
 
     registers = ['Gedackt8', 'PedGedackt8', 'Principal2', 'Rorflojt4',
                  'Salicional8', 'Subbas16', 'VoixCeleste8']
     p = re.compile('.*/(\d\d\d)-.*')
+
+
 
     with open(os.path.join(dir_out, 'instrument.txt'), 'w') as out:
         for ri, reg in enumerate(registers):
@@ -29,7 +32,8 @@ if __name__ == '__main__':
                 midi_note = p.findall(file_in)[0]
                 output_note = '%d%s.wav' % (ri, midi_note)
                 output_fname = os.path.join(dir_out, output_note)
-                proc = subprocess.Popen(['./looper', file_in, output_fname, '44100', '22050', '8192'],
+                proc = subprocess.Popen(['./looper', file_in, output_fname,
+                                         '22050', '66150', '5512'],
                                         stdout=subprocess.PIPE)
                 output, _ = proc.communicate()
                 print >> out, '%s %s %s' % (output_fname, output_note, output.strip())
